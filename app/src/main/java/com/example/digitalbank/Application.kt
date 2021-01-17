bd1d5dc9a617520b7d6d2eaf845bab42e295d062
Djeane Barros Carvalho <djeane_smlf@yahoo.com.br>
2021-01-17 14:40:40 -0300

    Creating feature modules

---
+++ app/src/main/java/com/example/digitalbank/Application.kt
@@ -0,0 +1,11 @@
+package com.example.digitalbank
+
+import android.app.Application
+import com.google.android.play.core.splitcompat.SplitCompatApplication
+
+class DigitalAccountApplication : SplitCompatApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
